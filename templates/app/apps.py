e1ab5cbfd454361b7a6a1dfd442110f82d1fb64e
Baskaran <baskaran@5gindia.net>
2016-12-01 20:14:16 +0530

    code

--- templates/app/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class TemplteTagsConfig(AppConfig):
-    name = 'app'
--- templates/tafe_dqms_template/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class TafeDqmsTemplateConfig(AppConfig):
-    name = 'tafe_dqms_template'
